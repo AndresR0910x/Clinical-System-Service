786641511f899c0efbdd5cb255c28417db889a55
bjsangoluisa <bjsangoluisa@espe.edu.ec>
2025-08-18 22:33:35 -0500

    Agrega service-registry y gateway, actualiza docker-compose.yml

---
+++ service-registry/src/test/java/com/root/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
